8f698f74d0504244a141e60dd4ef252546d4b3a6
ikeinyyo <ikeinyyo@outlook.com>
2025-03-09 02:38:32 +0100

    feat(run): Add run command to create alias to execute commands

---
+++ src/plugins/run/__init__.py
@@ -0,0 +1,9 @@
+from .run import run
+
+methods = {
+    "run": {
+        "method": run,
+        "info": "Manage and run to predefined command aliases.",
+        "usage": "ikein run [-a <alias> <command>] | [<alias>] | [-l] | [-r <alias>]",
+    }
+}
